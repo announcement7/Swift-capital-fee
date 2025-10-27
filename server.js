66318b720a334c86dc02342f777458205f4d35a7
announcement7 <masterbruce2023@gmail.com>
2025-10-27 08:40:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // ====== Configuration - update env or keep these for testing ======
-const PAYNECTA_EMAIL = process.env.PAYNECTA_EMAIL || "ceofreddy254@gmail.com";
-const PAYNECTA_API_KEY = process.env.PAYNECTA_API_KEY || "hmp_TfNKpPwUnjOkBd8g2TOBguJOiCmwWwv81Z3hHRPD";
-const PAYNECTA_CODE = process.env.PAYNECTA_CODE || "PNT_931740";
+const PAYNECTA_EMAIL = process.env.PAYNECTA_EMAIL || "imarikacos@gmail.com";
+const PAYNECTA_API_KEY = process.env.PAYNECTA_API_KEY || "hmp_ZYPdX5rx0LuCled8hKENf9mTZXDLnxI6uwVLNw5S";
+const PAYNECTA_CODE = process.env.PAYNECTA_CODE || "PNT_101521";
 
 // The callback URL PayNecta will call (use your tested backend domain)
 const CALLBACK_URL = process.env.CALLBACK_URL || "https://swiftcapitalportal.onrender.com";
