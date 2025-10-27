12def40fe25ccc8bdd3c25092265cffb4ef4df66
announcement7 <masterbruce2023@gmail.com>
2025-10-27 12:22:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const CALLBACK_URL = process.env.CALLBACK_URL || "https://swiftcapitalportal.onr
 const receiptsFile = path.join(__dirname, "receipts.json");
 
 // CORS origin: keep frontend intact
-const FRONTEND_ORIGIN = "https://techspacefinance.onrender.com";
+const FRONTEND_ORIGIN = "https://swiftcapitalportal.onrender.com";
 
 // Middleware
 app.use(bodyParser.json());
