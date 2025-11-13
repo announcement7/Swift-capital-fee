ef56ce458bd0f825ceb975f0770bc062c0a5bc1f
announcement7 <masterbruce2023@gmail.com>
2025-11-13 21:55:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,17 +13,17 @@ const PORT = process.env.PORT || 3000;
 
 // ====== Configuration - update env or keep these for testing ======
 const PAYNECTA_EMAIL = process.env.PAYNECTA_EMAIL || "imarikacos@gmail.com";
-const PAYNECTA_API_KEY = process.env.PAYNECTA_API_KEY || "hmp_qoqC9dZ5DCeZbAAxLdzlP6rVHoj2Pf5nybZZxNdn";
-const PAYNECTA_CODE = process.env.PAYNECTA_CODE || "PNT_255428";
+const PAYNECTA_API_KEY = process.env.PAYNECTA_API_KEY || "hmp_ZYPdX5rx0LuCled8hKENf9mTZXDLnxI6uwVLNw5S";
+const PAYNECTA_CODE = process.env.PAYNECTA_CODE || "PNT_101521";
 
 // The callback URL PayNecta will call (use your tested backend domain)
-const CALLBACK_URL = process.env.CALLBACK_URL || "https://swiftcapitalportal.onrender.com";
+const CALLBACK_URL = process.env.CALLBACK_URL || "https://swift-capital.onrender.com";
 
 // JSON storage file for receipts
 const receiptsFile = path.join(__dirname, "receipts.json");
 
 // CORS origin: keep frontend intact
-const FRONTEND_ORIGIN = "https://techspacefinance.onrender.com";
+const FRONTEND_ORIGIN = "https://swiftcapitalportal.onrender.com";
 
 // Middleware
 app.use(bodyParser.json());
